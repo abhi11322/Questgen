fa8f4a6a5362b1c3a65c4ac5ba8c30404e570857
Abhishek Poovaiah M <abhishekpoovaiah113@gmail.com>
2026-01-22 20:54:35 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- questgen-frontend/src/firebase.js
+++ questgen-frontend/src/firebase.js
@@ -2,13 +2,13 @@
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCbFBeGc56v1fLhsWeKqmf6JK0Z6Hc-sFU',
-  authDomain: 'questgen1-97446.firebaseapp.com',
-  projectId: 'questgen1-97446',
-  storageBucket: 'questgen1-97446.firebasestorage.app',
-  messagingSenderId: '410306860774',
-  appId: '1:410306860774:web:5a2bca42649137f5bd0d27',
-  measurementId: 'G-PHLZMSN6E9'
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 };
 
 const app = initializeApp(firebaseConfig);
